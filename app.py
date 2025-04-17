e72f76b8554eeb8a1d54abf742ad0b22eb2b150d
lee_kyoung_do <lkd3357@naver.com>
2025-04-17 13:12:16 +0900

    test

---
+++ app.py
@@ -0,0 +1,14 @@
+from server.flask_server import app as flask_app
+from server.dash_server import create_dash_app
+from server.thread_manager import run_multithread
+
+def run_flask():
+    flask_app.run(port=5050, debug=False, use_reloader=False)
+
+def run_dash():
+    create_dash_app().run(port=8050, debug=False, use_reloader=False)
+
+if __name__ == '__main__':
+    run_multithread(run_flask, run_dash)      
+    
+    
