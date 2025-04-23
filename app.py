90f300030b3c75834a437f98cc56b7d17a7d1a6d
leeKyoungdo <lkd3357@naver.com>
2025-04-24 00:53:14 +0900

    merge 과정에서 차이 있는것 최신화

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from server.flask_server import app as flask_app
 from server.dash_server import create_dash_app
 from server.thread_manager import run_multithread
-<<<<<<< HEAD
-=======
+
+
 from utils.config import SHARED
 
->>>>>>> kd
+
 
 def run_flask():
     flask_app.run(port=5050, debug=False, use_reloader=False)
@@ -14,10 +14,5 @@ def run_dash():
     create_dash_app().run(port=8050, debug=False, use_reloader=False)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    run_multithread(run_flask, run_dash)      
-    
-    
-=======
+
     run_multithread(run_flask, run_dash)
->>>>>>> kd
